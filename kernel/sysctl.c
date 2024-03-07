13bf8d88feed844a3d112e44023c98df5e598747
Ruchit <risenid@duck.com>
2024-03-07 17:17:05 +0530

    sched: guard sched_boost behind the proper guards
    Signed-off-by: Ruchit <risenid@duck.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -400,7 +400,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &sysctl_sched_group_upmigrate_pct,
 	},
-#if 0
+#ifdef CONFIG_SCHED_SEC_TASK_BOOST
 	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,

df02a9b877ba02278e56bd64c9fdc25f91c8d8bf
koko-07870 <ko282ko82@gmail.com>
2025-03-30 20:39:33 +0530

    regen.sh: only regenerate for a52q
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- regen.sh
+++ regen.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-rm -rf out
+regen_defconfig() {
+    echo "Generating defconfig for a52q"
+    make ARCH=arm64 O=out vendor/pixelos-a52q_defconfig
+    cp out/.config "arch/arm64/configs/vendor/pixelos-a52q_defconfig"
+    echo "defconfig regenerated for a52q"
+}
 
-# Array of device names
-devices=("a52q" "a72q")
-
-# Loop through the devices
-for device in "${devices[@]}"
-do
-    echo "Generating defconfig for $device"
-    make ARCH=arm64 O=out vendor/pixelos-"${device}"_defconfig
-    cp out/.config "arch/arm64/configs/vendor/pixelos-${device}_defconfig"
-    rm -rf out
-    echo "Defconfig generated for $device"
-    echo ""
-done
-
-echo "Defconfig generation completed for all devices."
-
-echo
+regen_defconfig

52ebec6813056385c8fa3335a26fe8de1af8f81d
FPSensor <gkartyt@gmail.com>
2026-07-04 13:32:43 +0000

    ckbuild: Update default Clang to AOSP Clang

--- ckbuild.sh
+++ ckbuild.sh
@@ -170,7 +170,7 @@ LOG_UPLOAD=1
 
 # Pick aosp, proton, rm69, lolz, slim, greenforce, zyc, rv, custom
 if [[ -z "$CLANG_TYPE" ]]; then
-    CLANG_TYPE="rm69"
+    CLANG_TYPE="aosp"
 else
     echo -e "\nINFO: Overriding default toolchain"
 fi
